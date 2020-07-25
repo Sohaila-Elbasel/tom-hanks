ea70bad0a800e8daaa816617082a2533bb9c89cb
sohaila-elbasel <sohailaelbasel@hotmail.com>
2020-07-25 21:56:19 +0200

    create movies page

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    data = []
     try:
-        data = []
         (cursor, connection) = connect()
         query = "SELECT name, pic_url, year, genre, rate FROM movies"
         cursor.execute(query)
@@ -19,6 +19,23 @@ def home():
     except Exception as error:
         print(error)
     return render_template('home.html', title='Tom Hanks', data = data)
+
+@app.route('/movies')
+def movies():
+    data = []
+    try:
+        (cursor, connection) = connect()
+        query = "SELECT * FROM movies;"
+        cursor.execute(query)
+        result = cursor.fetchall()
+        for row in result:
+            data.append(row)
+        close_connection(cursor, connection)
+
+    except Exception as error:
+        print(error)
+    finally:
+        return render_template('movies.html', title= 'Movies', data = data)
 
 if __name__ == '__main__':
     app.run()
--- connect_to_database.py
+++ connect_to_database.py
@@ -1,5 +1,20 @@
 import psycopg2
-from config import config
+from configparser import ConfigParser
+
+def config(filename='database.ini', section='postgresql'):
+    parser = ConfigParser()
+
+    parser.read(filename)
+    db = {}
+
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception(f'Section {section} not found in the {filename} file')
+
+    return db
 
 def close_connection(cursor, connection):
     try:
